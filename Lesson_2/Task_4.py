c6f6be299942ab8c7313cf87458205929691358a
LutovinovKN <VesperLKN@mail.ru>
2022-10-13 12:55:28 +0300

    Модернизировал 4 задачу

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -8,14 +8,24 @@ tmp = 0
 numbers = []
 for i in range(n):
     # *     Исключаем из рандома 0
-    while True:
-        tmp = randint(-n, n)
-        if tmp != 0: break
+    # while True:
+    #     tmp = randint(-n, n)
+    #     if tmp != 0: break
+    tmp = randint(-n, n) # Если хотите исключить 0 из списка, закомментируйте эту строку и раскоментируйте 3 предыдущие
     numbers.append(tmp)
 print(numbers)
 
 posit = []
-enter_posit = int(input(f"Введите, сколько позиций от 0 до {n} вы хотите выбрать? "))
+
+while True:
+    try:
+        enter_posit = int(input(f"Введите, сколько позиций от 0 до {n} вы хотите выбрать? "))      
+    except ValueError:
+        print("Not an integer!")
+        continue
+    else:
+        print("Yes an integer!")
+        break 
 for i in range(enter_posit):
     if enter_posit == 0:
         break
