b1e2aaa36a27ba99dcefdc7de8a13e59badae245
LutovinovKN <VesperLKN@mail.ru>
2022-10-17 12:10:18 +0300

    Выполнил задачу 2

--- Lesson_3/Task_2.py
+++ Lesson_3/Task_2.py
@@ -2,3 +2,28 @@
 #*     Пример:
 #*      [2, 3, 4, 5, 6] => [12, 15, 16];
 #*      [2, 3, 5, 6] => [12, 15]
+
+from random import randint
+from math import *
+while True:
+    try:
+        n = int(input('How long is the list you need? Enter a number: '))       
+    except ValueError:
+        print("Not an integer number!")
+        continue
+    else:
+        print("Yes an integer number!")
+        break 
+tmp = 0
+numbers = []
+for i in range(n):
+    tmp = randint(-9, 9)
+    numbers.append(tmp)
+print(numbers)
+
+composition_num = []
+half_list = ceil(n / 2)
+for i in range(half_list):
+    composition_num.append(numbers[i] * numbers[-(i+1)])
+    
+print("Произведением пар чисел списка numbers, является следующий список: ", composition_num)
