caeba8842b4e103016efd9e4ec9976af3546a1b8
LutovinovKN <VesperLKN@mail.ru>
2022-10-17 15:50:02 +0300

    Выполнил задачу 4

--- Lesson_3/Task_4.py
+++ Lesson_3/Task_4.py
@@ -3,3 +3,22 @@
 #*      45 -> 101101
 #*      3 -> 11
 #*      2 -> 10
+
+def inputNumber(msg):
+    while True:
+        try:
+            number = int(input(msg))
+            return number
+        except ValueError:
+            print("Вы не ввели число, попробуйте снова!")
+            break
+
+numb = inputNumber(msg = "Пожалуйста, введите целое число: ")
+
+res = ''
+
+while numb:
+    res = str(numb % 2) + res
+    numb = numb // 2
+
+print(res if res else '0')
