2b76699b079442bb08472ff3c07e656b1516bf6f
LutovinovKN <VesperLKN@mail.ru>
2022-10-21 12:40:53 +0300

    выполнены 1 и 2 задачи

--- Lesson_4/Task_1.py
+++ Lesson_4/Task_1.py
@@ -6,23 +6,30 @@
 #?   при d = 0.00001, π = 3.14154
 #?   d от 0.1 до 0.0000000001
 # !!! не округлять константу math
-from decimal import Decimal
+from decimal import *
 from math import *
 from unicodedata import decimal
-d = input('Введите число в пределах от 0.1 до 0.0000000001: ')
-# while True:
-#         try:
-#             d = abs(Decimal(input('Введите число в пределах от 0.1 до 0.0000000001: ')))
-#             break 
-#         except ValueError:
-#             print("Not an float number!")
-#             continue
+# d = input('Введите число в пределах от 0.1 до 0.0000000001: ')
+from decimal import Decimal
+while True:
+        try:
+            d = abs(Decimal(input('Введите число в пределах от 0.1 до 0.0000000001: ')))
+            break 
+        except ValueError:
+            print("Not an float number!")
+            continue
+s = '{0:f}'.format(d)
+len_s = len(s) - s.index('.')
 
 num_pi = asin(sqrt(1 - 1**2))+ abs(asin(1)) * 2
 print(num_pi)
+
 if num_pi == pi:
     print("Число Пи найдено верно!")
 else:
     print("Число Пи найдено неверно!")
-print(round(num_pi, len(str(d))-2))
+round_pi = []
+for i in range(len(str(num_pi))):
+    round_pi.extend(str(num_pi)[i])
 
+print(*round_pi[:len_s + 1], sep='')
