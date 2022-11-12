5caf2193d34d17203253ff2f998cbce750f91392
LutovinovKN <VesperLKN@mail.ru>
2022-11-12 20:54:17 +0300

    всё та же рыба

--- Lesson_7/Telephone directory/main.py
+++ Lesson_7/Telephone directory/main.py
@@ -1,3 +1,10 @@
 import controller
 
 controller.start()
+
+from controller import get_controller
+def main():
+    None
+
+if __name__ == "__main__":
+    main()
--- Lesson_7/Telephone directory/model.py
+++ Lesson_7/Telephone directory/model.py
@@ -1,46 +1,46 @@
 
 
-
-#   Фамилия
-def last_name():
-    None
-    
-    
-#   Имя
-def  first_name():
-    None
-    
-    
-#   Отчество
-def patronymic():
-    None
-    
-    
-#   Работа
-def work():
-    None
-    
-    
-#   Номер телефона
-def number_telephone():
-    None
-    
-    
-#   Группа
-def group():
-    None
-    
-    
-#   Почта
-def email():
-    None
-    
-    
-#   Важная дата
-def important_date():
-    None
-    
-    
-#   Веб-сайт
-def website():
-    None
+class User():
+    #   Фамилия
+    def last_name():
+        None
+        
+        
+    #   Имя
+    def  first_name():
+        None
+        
+        
+    #   Отчество
+    def patronymic():
+        None
+        
+        
+    #   Работа
+    def work():
+        None
+        
+        
+    #   Номер телефона
+    def number_telephone():
+        None
+        
+        
+    #   Группа
+    def group():
+        None
+        
+        
+    #   Почта
+    def email():
+        None
+        
+        
+    #   Важная дата
+    def important_date():
+        None
+        
+        
+    #   Веб-сайт
+    def website():
+        None
