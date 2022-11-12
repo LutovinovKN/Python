1271d4732eb71e34c80687720452b537a23989b3
Elvira <eliasufa@yahoo.com>
2022-11-13 02:15:15 +0500

    Описаны меню пользователя и функция проверки на ввод числа

--- Lesson_7/Telephone directory/view.py
+++ Lesson_7/Telephone directory/view.py
@@ -4,3 +4,24 @@ def showinfo(input_value):
 
 def getNumb(output_value):
     return input(output_value) 
+
+
+def showMenu():
+    print('\nГлавное меню:\n'
+          '1. Открыть файл\n'
+          '2. Сохранить файл\n'
+          '3. Показать все контакты\n'
+          '4. Поиск контакта\n'
+          '5. Добавить контакт\n'
+          '6. Изменить контакт\n'
+          '7. Удалить контакт\n'
+          '0. Выход')
+
+
+def inputInt(text):
+      while True:
+            try:
+                  number = int(input(text))
+                  return number
+            except:
+                  print('Ошибка! Введите целое число!')
