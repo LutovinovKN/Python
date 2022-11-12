397ab53c1dbd31dcdb4caa087e901de4c883f900
LutovinovKN <VesperLKN@mail.ru>
2022-11-12 19:44:26 +0300

    рыба

---
+++ Lesson_7/Telephone directory/model.py
@@ -0,0 +1,34 @@
+
+
+
+
+def last_name():
+    None
+    
+    
+def  first_name():
+    None
+    
+    
+def patronymic():
+    None
+    
+    
+def work():
+    None
+    
+    
+def number_telephone():
+    None
+    
+    
+def email():
+    None
+    
+    
+def important_date():
+    None
+    
+    
+def website():
+    None
