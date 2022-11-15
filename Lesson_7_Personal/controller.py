58a7dde090b8aaa6dea51f2937b2bc61a806bd19
LutovinovKN <VesperLKN@mail.ru>
2022-11-15 23:02:03 +0300

    Немного дописал контроллер

--- Lesson_7_Personal/controller.py
+++ Lesson_7_Personal/controller.py
@@ -13,18 +13,20 @@ def run():
         match choise_menu:
             case '1':
                 view.show_contact_list() # Показыает список контактов
-                view.show_contact_menu()
-                choise_show_contact_menu = view.get_input("Выберите позицию: ")
-                
                 runing_choise_show_contact = True
                 while runing_choise_show_contact:
+                    view.show_contact_menu()
+                    choise_show_contact_menu = view.get_input("Выберите позицию: ")
+                
+                
                     match choise_show_contact_menu:
                         case '1':
                             id_contact = view.get_input("Введите id контакта: ")
                             
                             view.contact_data_entry_menu()
                             choise_contact_data_entry_menu = view.get_input("Выберите позицию: ")
-                            
+                        case 'back':
+                            runing_choise_show_contact = False
                             match choise_contact_data_entry_menu:
                                 case '1':
                                     model.change_field("name")
@@ -34,18 +36,29 @@ def run():
                                     model.change_field("number")
                                 case '4':
                                     model.change_field("number+")
-                                case '5':
-                                    model.funct_back("back")
-                        # case '2':
-                        # case '3':
+                                case '9':
+                                    model.del_contact
+                                case 'back':
+                                    runing_choise_show_contact = False
                         
                 # view.choise_contact() # Выбор контакта
                 # view.find_contact()
             case '2':
-                None
+                view.contact_add_menu() # Показывает меню создания контакта
+                runing_add_contact = True
+                while runing_add_contact:
+                    choise_add_contact = view.get_input("Выберите позицию: ")
+                    
+                    match choise_add_contact:
+                        case '1':
+                            model.change_field("name")
+                        case '2':
+                            model.change_field("surname")
+                        case '3':
+                            model.change_field("number")
+                        case '4':
+                            model.change_field("number+")
             case '3':
-                None
-            case '4':
                 None
             case 'exit':
                 running = False
